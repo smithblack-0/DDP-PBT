b5708c3261af279b3b1a5bbf4ba4beba7760a1ff
chris <chrisoquinn.2@gmail.com>
2026-01-20 14:50:59 -0800

    Setting up and moving the strategy into the strategies folder.

--- src/ddp_pbt/Strategies/top_score_strategy.py
+++ src/ddp_pbt/Strategies/top_score_strategy.py
@@ -8,10 +8,10 @@ adopts their hyperparameters (with perturbation), model, and optimizer state.
 from typing import List, Dict, Optional, Any
 import torch
 
-from src.ddp_pbt.base.abstract_strategy import AbstractStrategy
-from src.ddp_pbt.base.state import State
-from src.ddp_pbt.base.communication import Communication
-from src.ddp_pbt.base.perturber import Perturber
+from ..base.abstract_strategy import AbstractStrategy
+from ..base.state import State
+from ..base.communication import Communication
+from ..base.perturber import Perturber
 
 
 class TopScoreStrategy(AbstractStrategy):
